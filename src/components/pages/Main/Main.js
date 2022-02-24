abeddad01498fb46d118690212bb45c647def11e
sueyon-hong <bellahong95@gmail.com>
2022-02-24 20:05:55 +0900

    :sparkles: Feat: Main 컴포넌트 구현

---
+++ src/components/pages/Main/Main.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import * as S from './Style'
+
+export default function Main() {
+	const INTRO_TEXT = '안녕하세요. 신입 프론트엔드 개발자 홍수연 입니다.'
+	const [text, setText] = useState('')
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setText(text => text + INTRO_TEXT[count])
+			setCount(count => count + 1)
+		}, 150)
+
+		count === INTRO_TEXT.length && clearInterval(interval)
+		return () => clearInterval(interval)
+	}, [count])
+
+	return (
+		<S.Figure>
+			<S.TextWrapper>{text}</S.TextWrapper>
+		</S.Figure>
+	)
+}

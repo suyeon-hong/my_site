49a37a64ed954bd373568922911a4a10f7acf4b4
sueyon-hong <bellahong95@gmail.com>
2022-02-24 15:29:01 +0900

    :sparkles: Feat: Contact 컴포넌트 구현

---
+++ src/components/pages/Contact/Contact.js
@@ -0,0 +1,56 @@
+import * as S from './Style'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+export default function Contact() {
+	const form = useRef()
+
+	const sendEmail = e => {
+		e.preventDefault()
+
+		emailjs
+			.sendForm(
+				process.env.REACT_APP_SERVICE_ID,
+				process.env.REACT_APP_TEMPLATE_ID,
+				form.current,
+				process.env.REACT_APP_USER_ID,
+			)
+			.then(() => {
+				form.current.querySelectorAll('input').forEach(input => (input.value = ''))
+				form.current.querySelector('textarea').value = ''
+				alert('메일 전송이 완료되었습니다.')
+			})
+	}
+
+	return (
+		<S.PageWrapper>
+			<S.Form ref={form} onSubmit={sendEmail}>
+				<S.InputWrapper>
+					<label htmlFor="name">Name</label>
+					<input
+						name="name"
+						id="name"
+						type="text"
+						placeholder="이름을 입력해 주세요"
+						maxLength={10}
+					/>
+				</S.InputWrapper>
+				<S.InputWrapper>
+					<label htmlFor="email">E-mail</label>
+					<input
+						name="email"
+						id="email"
+						type="email"
+						placeholder="이메일 주소를 입력해 주세요"
+						required
+					/>
+				</S.InputWrapper>
+				<S.InputWrapper>
+					<label htmlFor="message">Message</label>
+					<textarea name="message" id="message" placeholder="메세지를 입력해 주세요" required />
+				</S.InputWrapper>
+				<button type="submit">Send</button>
+			</S.Form>
+		</S.PageWrapper>
+	)
+}
---
+++ src/components/pages/Contact/Style.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+import { hideScrollbar } from 'styles/CommonStyle'
+
+export const PageWrapper = styled.main`
+	width: 100%;
+	height: calc(100vh - 200px);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const Form = styled.form`
+	padding: ${({ theme }) => `${theme.base.padding * 2}px`};
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-end;
+	gap: ${({ theme }) => `${theme.base.padding * 2}px`};
+
+	button {
+		width: 90px;
+		height: 45px;
+		font-size: 16px;
+		line-height: 45px;
+		text-align: center;
+		background: ${({ theme }) => theme.colors.lightGray};
+		border-radius: 2px;
+		transition: 0.6s ease;
+
+		&:hover {
+			background: ${({ theme }) => theme.colors.darkGray};
+			color: ${({ theme }) => theme.colors.white};
+		}
+	}
+`
+
+export const InputWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	gap: ${({ theme }) => `${theme.base.padding * 2}px`};
+
+	label {
+		font-size: 18px;
+	}
+
+	input,
+	textarea {
+		width: 60vw;
+		height: 50px;
+		border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+		padding: ${({ theme }) => `0 ${theme.base.padding}px`};
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 1.4;
+		color: ${({ theme }) => theme.colors.darkGray};
+	}
+
+	textarea {
+		height: 200px;
+		overflow-y: auto;
+		border: 1px solid ${({ theme }) => theme.colors.lightGray};
+		border-radius: 2px;
+		padding: ${({ theme }) => `${theme.base.padding}px`};
+		resize: none;
+		${hideScrollbar};
+	}
+`

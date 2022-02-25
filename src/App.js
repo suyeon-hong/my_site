f375f3e5f0e99c790e5a74725c55633d21d2e49c
sueyon-hong <bellahong95@gmail.com>
2022-02-25 16:43:20 +0900

    :sparkles: Feat: page Router 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/Global";
-import theme from "./styles/theme";
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/GlobalStyle'
+import theme from './styles/theme'
+import { Header, Footer } from 'components/domain'
+import { Routes, Route } from 'react-router-dom'
+import { Main, About, Contact, Skills, Portfolio } from 'components/pages'
 
 function App() {
 	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<GlobalStyles />
-				<div>hi</div>
-			</ThemeProvider>
-		</>
-	);
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<Header />
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/skills" element={<Skills />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+			<Footer />
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App

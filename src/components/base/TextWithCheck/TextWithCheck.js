5d68cccf2bcdd8a31a4709ea150d59474d1c185c
sueyon-hong <bellahong95@gmail.com>
2022-02-25 12:12:48 +0900

    :recycle: Refactor: propTypes 추가

--- src/components/base/TextWithCheck/TextWithCheck.js
+++ src/components/base/TextWithCheck/TextWithCheck.js
@@ -1,4 +1,9 @@
 import * as S from './Style'
+import PropTypes from 'prop-types'
+
+TextWithCheck.propTypes = {
+	children: PropTypes.string.isRequired,
+}
 
 export default function TextWithCheck({ children }) {
 	return <S.Text>✔ {children}</S.Text>

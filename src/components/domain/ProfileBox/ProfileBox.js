cf2b5f590305774609f73da5aca659f08bc53280
suyeon-hong <bellahong95@gmail.com>
2022-03-23 20:22:53 +0900

    :bento: Assets: profile이미지 등록

--- src/components/domain/ProfileBox/ProfileBox.js
+++ src/components/domain/ProfileBox/ProfileBox.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import * as S from './Style'
 import { TextWithCheck, IconBox } from 'components/base'
+import { profile } from 'assets/images'
 
 ProfileBox.propTypes = {
 	profileInfo: PropTypes.object.isRequired,
@@ -12,7 +13,9 @@ export default function ProfileBox({ profileInfo }) {
 
 	return (
 		<S.ProfileBox>
-			<S.ImgBox />
+			<S.ImgBox>
+				<img src={profile} alt="profile" />
+			</S.ImgBox>
 			<div>
 				<IconBox icons={icons} />
 				{text && text.map((el, index) => <TextWithCheck key={index} children={el} />)}

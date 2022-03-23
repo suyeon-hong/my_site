a903d153f09fe1a861a72fc3c9ef18edc4350e81
suyeon-hong <bellahong95@gmail.com>
2022-03-23 20:26:19 +0900

    :sparkles: Feat: Main페이지 포트폴리오 추가

---
+++ src/pages/Main/Style.js
@@ -0,0 +1,43 @@
+import styled, { keyframes } from 'styled-components'
+import { figure } from 'assets/images'
+import { pageInner } from 'styles/CommonStyle'
+
+export const Figure = styled.figure`
+	width: 100%;
+	height: calc(100vh - 80px);
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	position: relative;
+	background: url(${figure}) no-repeat center/cover;
+	padding-top: 300px;
+`
+
+const cursor = keyframes`
+	0% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0;
+	}
+`
+
+export const TextWrapper = styled.div`
+	font-size: 20px;
+	color: ${({ theme }) => theme.colors.lightGray};
+
+	&::after {
+		content: '';
+		display: inline-block;
+		width: 1px;
+		height: 24px;
+		background: ${({ theme }) => theme.colors.darkGray};
+		animation: ${cursor} 0.8s infinite;
+		vertical-align: sub;
+		margin-left: ${({ theme }) => `${theme.base.padding / 2}px`};
+	}
+`
+
+export const PageInner = styled.div`
+	${pageInner};
+`
